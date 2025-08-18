090628afca97504bbd4111efbbeb37919f68650c
Manikanth8447 <91658690+Manikanth8447@users.noreply.github.com>
2025-08-18 17:53:39 +0530

    Add files via upload

---
+++ src/main/java/com/mani/MyDemoWebAppMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.mani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDemoWebAppMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDemoWebAppMySqlApplication.class, args);
+	}
+
+}
